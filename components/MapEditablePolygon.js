b3e9531d40c4411312bfe29781001a44bcbe0fc3
Scott Fisher <sfisher@airware.com>
2017-09-28 14:23:38 -0700

    Change PropTypes access
    RN 0.40+

--- components/MapEditablePolygon.js
+++ components/MapEditablePolygon.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { View, Platform } from 'react-native';
 import MapView from 'react-native-maps';
 import geoUtils from './utils/geoUtils';
